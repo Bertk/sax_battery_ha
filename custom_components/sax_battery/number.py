3845e8697f40152409cb193553be478e513f396a
Bert <bert.k@hotmail.com>
2026-08-09 08:57:08 +0000

    Implement SunSpec mode support

--- custom_components/sax_battery/number.py
+++ custom_components/sax_battery/number.py
@@ -31,7 +31,6 @@ from .const import (
     LIMIT_MAX_CHARGE_PER_BATTERY,
     LIMIT_MAX_DISCHARGE_PER_BATTERY,
     LIMIT_REFRESH_INTERVAL,
-    MODBUS_BATTERY_POWER_CONTROL_ITEMS,
     REFRESH_REGISTERS,
     SAX_COMBINED_SOC,
     SAX_MAX_CHARGE,
@@ -42,6 +41,7 @@ from .const import (
     SAX_NOMINAL_POWER,
     WRITE_ONLY_REGISTERS,
 )
+from .const_legacy import MODBUS_BATTERY_POWER_CONTROL_ITEMS
 from .coordinator import SAXBatteryCoordinator
 from .entity_utils import filter_items_by_type, filter_sax_items_by_type
 from .enums import TypeConstants
